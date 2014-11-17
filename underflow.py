9fe089f6786292aa88142353a562ce8e276ab653
Bradley Barrios <bradley.barrios@yahoo.com>
2014-11-17 13:36:02 -0600

    I <3 Joel

--- underflow.py
+++ underflow.py
@@ -231,8 +231,8 @@ def test_mlp(learning_rate=0.01, L1_reg=0.00, L2_reg=0.0001, n_epochs=100, batch
             print(str(minibatch_average_cost) + " " + str(epoch) + " \n")
 
 
-	#output = train_model(tempx, tempy)
-	#print str(output)
+	output = train_model(tempx, tempy)
+	print str(output)
 
 if __name__ == '__main__':
 	test_mlp()
